1a79a36cc5ea36be6e8ef01e94f40180b7e99dd8
godori004 <godori004@naver.com>
2020-09-10 14:43:07 +0900

    테스트 날씨 api

--- src/stock/myStockInfo.py
+++ src/stock/myStockInfo.py
@@ -3,13 +3,13 @@ from src.stock import procStock, corpInfo
 
 
 def getMyStockInfo():
-    df = pd.read_csv("C:/Users/DAL/Documents/GitHub/Pycharm/src/stock/resource/myStockInfo", index_col='index')
+    df = pd.read_csv("resource/myStockInfo", index_col='index')
     return df
 
 def getMyStockObject(*args):
 
     if len(args)==0:
-        nameArr = ('한국공항', '롯데하이마트', '랩지노믹스', '메지온', 'PI첨단소재', '세틀뱅크', '윙입푸드', '에스씨엠생명과학')
+        nameArr = ('한국공항', '롯데하이마트', '현대로템', 'PI첨단소재', '세틀뱅크', '윙입푸드', '에스씨엠생명과학', '수산중공업', '녹십자엠에스')
     elif len(args)>1:
         raise ValueError('인자가 너무 많습니다.')
     elif type(args[0])!=tuple:
